92296eaa8bd3f13c4355166c0fed291e43474f86
rex-smith <rex.smith17@gmail.com>
2022-05-30 21:10:01 -0400

    Added controller actions for Events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,49 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit]
   def index
     @events = Event.all.order('date ASC')
   end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = current_user.events.build
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to root_path, notice: "Event was successfully created." }
+        format.json { render :index, status: :created, location: @event }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @event.update(event_params)
+        format.html { redirect_to post_url(@event), notice: "Event was successfully updated." }
+        format.json { render :show, status: :ok, location: @event }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+    def event_params
+      params.require(:event).permit(:name, :date)
+    end
 end
