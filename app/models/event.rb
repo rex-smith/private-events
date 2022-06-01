866b63ad6b2053a67522cb747583eaeb3364a896
rex-smith <rex.smith17@gmail.com>
2022-06-01 10:35:57 -0400

    Added scopes for past and future events

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,12 +3,15 @@ class Event < ApplicationRecord
   has_many :attendances, foreign_key: :attended_event_id
   has_many :attendees, through: :attendances
 
-  def self.past
-    @events = Event.where('date < ?', DateTime.now)
-  end
+  # def self.past
+  #   @events = Event.where('date < ?', DateTime.now)
+  # end
 
-  def self.future
-    @events = Event.where('date >= ?', DateTime.now)
-  end
+  # def self.future
+  #   @events = Event.where('date >= ?', DateTime.now)
+  # end
+
+  scope :past, -> { where('date < ?', DateTime.now) }
+  scope :future, -> { where('date >= ?', DateTime.now )}
 
 end
