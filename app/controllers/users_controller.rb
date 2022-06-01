81d51c0c22b19340b82d1dd46c207b61ceeeafcd
rex-smith <rex.smith17@gmail.com>
2022-05-31 23:51:45 -0400

    Separated upcoming from past events for users show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @events = current_user.created_events
+    
+    @created_events = current_user.created_events
+    @attended_events = current_user.attended_events
   end
 end
